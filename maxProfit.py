c6b38fcad7627bfae769b91852d2ca004e8ea024
Tyler Bowen <tbowen8615@gmail.com>
2025-01-19 12:23:57 -0600

    changed max profit

--- maxProfit.py
+++ maxProfit.py
@@ -3,10 +3,10 @@ def maxProfit(prices):
     max_profit = 0
 
     for price in prices:
-        min_price = min(min_price, price)
-        profit = price - min_price
-        max_profit = max(max_profit, profit)
-
+        if price < min_price:
+            min_price = price
+        elif price - min_price > max_profit:
+            max_profit = price - min_price
     print(max_profit)
     return max_profit
 
